766469480f16d98032de4a7a23aaaadcc43bff96
rilmayer <yessenbayev.chingis@gmail.com>
2025-10-17 19:56:52 +0500

    JAVA-DISC: реализация Deadlock

--- src/main/java/kz/shyngys/proselyte_course/module_1_4/threads/Deadlock.java
+++ src/main/java/kz/shyngys/proselyte_course/module_1_4/threads/Deadlock.java
@@ -1,2 +1,60 @@
 package kz.shyngys.proselyte_course.module_1_4.threads;
 
+class A {
+    synchronized void foo(B b) {
+        String name = Thread.currentThread().getName();
+        System.out.println(name + " зашел в метод foo()");
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            System.out.println(e);
+        }
+        System.out.println(name + " пытается вызвать метод b.last()");
+        b.last();
+    }
+
+    synchronized void last() {
+        System.out.println("A last");
+    }
+
+}
+
+class B {
+    synchronized void bar(A a) {
+        String name = Thread.currentThread().getName();
+        System.out.println(name + " зашел в метод bar()");
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            System.out.println(e);
+        }
+        System.out.println(name + " пытается вызвать a.last()");
+        a.last();
+    }
+
+    synchronized void last() {
+        System.out.println("B last");
+    }
+}
+
+public class Deadlock implements Runnable {
+    A a = new A();
+    B b = new B();
+    Thread t;
+
+    public Deadlock() {
+        Thread.currentThread().setName("Главный поток");
+        t = new Thread(this, "Побочный поток");
+        t.start();
+        a.foo(b);
+    }
+
+    @Override
+    public void run() {
+        b.bar(a);
+    }
+
+    public static void main(String[] args) {
+        new Deadlock();
+    }
+}
